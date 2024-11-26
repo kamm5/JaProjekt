dcab2668cbb8c0c95b830d1ed4bc6297a4a89a04
KamoPL 155 <kamilx155x@gmail.com>
2024-11-26 14:43:16 +0100

    create main menu v1

--- JaProjekt/MainWindow.xaml.cs
+++ JaProjekt/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,12 +25,75 @@ namespace JaProjekt
         [DllImport(@"..\..\..\..\..\x64\Debug\JaCpp.dll")]
         static extern int Myproc(int a, int b);
 
+        public byte[,,] pixelArray;
+
+        private void PickFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Tworzymy dialog do wyboru pliku
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Pliki BMP (*.bmp)|*.bmp"
+            };
+
+            // Pokazujemy okno dialogowe
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    // Wczytanie obrazu BMP do ImageViewer
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.UriSource = new Uri(filePath);
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.EndInit();
+                    ImageViewer.Source = bitmapImage;
+
+                    // Wczytanie obrazu BMP do tablicy pikseli
+                    Bitmap bitmap = new Bitmap(filePath);
+                    pixelArray = ConvertBitmapToRGBArray(bitmap);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Błąd podczas wczytywania obrazu: {ex.Message}",
+                                    "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                MessageBox.Show(pixelArray[1, 0, 0].ToString() + "," + pixelArray[1, 0, 1].ToString() + "," + pixelArray[1, 0, 2].ToString());
+            }
+        }
+
+        private byte[,,] ConvertBitmapToRGBArray(Bitmap bitmap)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            // Tablica trójwymiarowa do przechowywania wartości RGB
+            byte[,,] pixelArray = new byte[height, width, 3];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Pobieramy kolor piksela
+                    System.Drawing.Color pixelColor = bitmap.GetPixel(x, y);
+
+                    // Wypełniamy wartości RGB
+                    pixelArray[y, x, 0] = pixelColor.R; // Red
+                    pixelArray[y, x, 1] = pixelColor.G; // Green
+                    pixelArray[y, x, 2] = pixelColor.B; // Blue
+                }
+            }
+
+            return pixelArray;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            int x = 5, y = 3;
-            int retVal = MyProc1(x, y);
-            MessageBox.Show(retVal.ToString());
+            //int x = 5, y = 3;
+            //int retVal = MyProc1(x, y);
+            //MessageBox.Show(retVal.ToString());
         }
     }
 }
